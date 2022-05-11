aea961ed21dee2ecb7d67e84ef9edb63672decc1
sunmi <red@wordncode.com>
2022-05-11 18:53:41 +0900

    0511

---
+++ html/resource/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    se1Box1_Swiper();
+    se1Box2_Swiper();
+});    
+
+function se1Box1_Swiper(){
+    var se1Box1Swiper = new Swiper(".se1Box1 .mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".se1Box1 .swiper-button-next",
+          prevEl: ".se1Box1 .swiper-button-prev",
+        },
+      });
+}
+function se1Box2_Swiper(){
+    var se1Box2Swiper = new Swiper(".se1Box2 .mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".se1Box2 .swiper-button-next",
+          prevEl: ".se1Box2 .swiper-button-prev",
+        },
+      });
+}
